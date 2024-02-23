43c416e504e71c131e0a25e11c7ea6013a1b892d
Christos Christoforos Mitsakopoulos <mitsacopoulos@gmail.com>
2024-02-23 15:05:30 +0000

    Final

--- Variant-calling-automation.py
+++ Variant-calling-automation.py
@@ -10,22 +10,24 @@ if os.path.exists(os.path.join(main_directory, "RESULTS.txt")):
 main_dir_indexes = [y for y in os.listdir(main_directory) if not "RESULTS" in y]
 for item in main_dir_indexes:
     extended_path = os.path.join(main_directory, item)
-    list_of_dirs = [x for x in os.listdir(extended_path) if ".vcf" in x]
+    list_of_dirs = [x for x in os.listdir(extended_path) if ".vcf" in x and "pilon" in x]
     for index in list_of_dirs:
-        specific_file = os.path.join(extended_path, index)
-        cmd = f"perl /home/student/resources/workshops/VCF_summary.pl {specific_file}"
-        if "filtered" in index:
+        if "filtered" in str(index):
+            specific_file = os.path.join(extended_path, index)
+            cmd = f"perl /home/student/resources/workshops/VCF_summary.pl {specific_file}"
             amogus = subprocess.run(cmd, shell=True, capture_output=True, cwd=extended_path)
             with open(os.path.join(main_directory, "RESULTS.txt"), 'a') as output:
                output.write(f"{item}\n{amogus}\n")
         else:
             extra = '(LowCov|Amb|Del)'
+            specific_file_2 = os.path.join(extended_path, index)
             final_exit = os.path.join(extended_path, f"{item}-bwa-mem.sorted.bam.pilon-filtered.vcf")
             if not os.path.exists(final_exit):
                 with open(final_exit, 'w') as one:
                     pass
-            directive = f"grep -vwE {shlex.quote(extra)} {specific_file} > {final_exit}"
+            directive = f"grep -vwE {shlex.quote(extra)} {specific_file_2} > {final_exit}"
             subprocess.run(directive, cwd=extended_path, shell=True)
-            amogusus = subprocess.run(cmd, shell=True, capture_output=True, cwd=extended_path)
+            cmd_2 = f"perl /home/student/resources/workshops/VCF_summary.pl {final_exit}"
+            amogusus = subprocess.run(cmd_2, shell=True, capture_output=True, cwd=extended_path)
             with open(os.path.join(main_directory, "RESULTS.txt"), 'a') as output:
                 output.write(f"{item}\n{amogusus}\n")
